d890426852969ea5e74bce2947e200c3d0e4cd54
radu.lung <radu.lung@tss-yonder.com>
2021-03-02 22:40:09 +0200

    Implement Substract operation

---
+++ SubstractOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace Submodule2
+{
+    public class SubstractOperation
+    {
+        public int Execute(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
